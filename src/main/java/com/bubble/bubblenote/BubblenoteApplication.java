87ed41e9c0cc71255e1e46a62fc1f6b02767bd38
TheMechanist001 <priti_bhowmik@live.com>
2020-03-06 15:47:51 -0600

    Initial commit

---
+++ src/main/java/com/bubble/bubblenote/BubblenoteApplication.java
@@ -0,0 +1,13 @@
+package com.bubble.bubblenote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BubblenoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BubblenoteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bubble/bubblenote/BubblenoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bubble.bubblenote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BubblenoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
